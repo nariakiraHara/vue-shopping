f84422ada8848efd1d5a1aec0fd5d2783650d7a3
TKY\nariakirahara <nariakirahara@carview.co.jp>
2020-05-09 13:29:24 +0900

    クラス名変更

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="shopping">
+    <ItemList msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import ItemList from '@/components/ItemList.vue'
 
 export default {
-  name: 'Home',
+  name: 'Shopping',
   components: {
-    HelloWorld
+    ItemList
   }
 }
 </script>
